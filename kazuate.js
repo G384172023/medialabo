0a5f0e134380970ef51e139507e948907164e38d
G384172023 <238417@st.takushoku-u.ac.jp>
2024-05-30 15:02:30 +0900

    数当て

--- kazuate.js
+++ kazuate.js
@@ -1,38 +1,41 @@
 let kotae=Math.floor(Math.random()*10)+1;
-console.log('答え(デバック用) : '+kotae);
-
 let kaisu=0;
 
-hantei();
-
 function hantei(){
-    let yoso=4;
-    for(let i=1;i<3;i++){
-        console.log(i+'回目の予想: '+yoso);
-        if(kotae==yoso && i==2){
-            console.log('答えは '+kotae+' でした.すでにゲームは終わっています');
-        }
-        if(kotae==yoso && i==1){
-            console.log('正解です.おめでとう!');
-        }else if(kotae>yoso){
-            console.log('まちがい、答えはもっと大きいですよ');
-        }else if(kotae<yoso){
-            console.log('まちがい、答えはもっと小さいですよ');
-        }
+    let i=document.querySelector('input[name="shimei"]');
+    let yoso=i.value;
+    let A;
 
-    }
-    console.log('3回目の予想: '+yoso);
-    if(kotae==yoso){
-        console.log('答えは '+kotae+' でした.すでにゲームは終わっています');
+    kaisu += 1;
+    console.log(kaisu+'回目の予想:'+yoso);
+    A=(kaisu+'回目の予想:'+yoso);
+
+    if(kaisu>=4){
+        console.log("答えは "+kotae+" でした.すでにゲームは終わっています");
+        A=("答えは "+kotae+" でした.すでにゲームは終わっています");
+    }else if(kotae==Number(yoso)){
+        console.log("正解です. おめでとう！");
+        A=("正解です. おめでとう！");
+    }else if(kotae==3){
+        console.log("まちがい.残念でした. 答えは "+kotae+" です.");
+        A=("まちがい.残念でした. 答えは "+kotae+" です.");
     }else{
-        console.log('まちがい、残念でした答えは '+kotae+' です.');
+        if(yoso<kotae){
+            console.log("まちがい. 答えはもっと大きいですよ");
+            A=("まちがい. 答えはもっと大きいですよ");
+        }else{
+            console.log("まちがい. 答えはもっと小さいですよ");
+            A=("まちがい. 答えはもっと小さいですよ");
+        }
     }
-    for(let i=4;i<10;i++){
-        console.log(i+'回目の予想: '+yoso);
-        console.log('答えは '+kotae+' でした.すでにゲームは終わっています');
-    }
+
+    let x1=document.querySelector('span#kaisu');
+    x1.textContent=kaisu;
+    let x2=document.querySelector('span#answer');
+    x2.textContent=yoso;
+
+    let y1=document.querySelector('p#result');
+    y1.textContent=A;
 }
-
-
-
-
+let z1=document.querySelector('button');
+z1.addEventListener('click',hantei);
